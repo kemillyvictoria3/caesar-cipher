d2fe8d7b48ef058e92a9625d5d234444bc1f1ebb
kemilly <kemillyvictoriapereirarocha@gmail.com>
2024-06-06 12:22:26 -0300

    aula 06/06/20024

--- app/__init__.py
+++ app/__init__.py
@@ -4,29 +4,32 @@ from app.encode import encode_message
 def create_app():
     #Create and configure the app
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'asbvvjhbsjhghsgbkhjckhgckjhgskk'
+    app.config['SECRET_KEY'] = 'asbvjadbfvskhjdhfkjsdgsdgb'
 
-    @app.route("/")
+    @app.route('/')
     def home(name='Home'):
         return render_template('index.html', name=name)
 
-    @app.route("/encode" ,methods=('get', 'POST'))
+    @app.route('/encode', methods=('GET', 'POST'))
     def encode(name='Encode'):
         if request.method == 'POST':
             try:
                 cipher_key = int(request.form['cipher_key'])
             except:
                 cipher_key = 0
-
+            
             message = request.form['message']
 
-            if not cipher_key:
+            if cipher_key == 0:
+                flash("A chave deve ser um número!")
+            elif not cipher_key:
                 flash('A chave é obrigatória...')
-            elif not message:
-                flash('a mensagem é necessária...')
+            elif not message :
+                flash('A mensagem é necessária...')
             else:
-                flash('ENCODE')
-  
-        return render_template('encode.html', name=name)
+                encoded_message = encode_message(message, int(cipher_key))
+                flash(encoded_message)
+
+        return render_template('encode.html',name=name)
     
     return app
--- app/encode/__init__.py
+++ app/encode/__init__.py
@@ -1,27 +1,26 @@
 def encode_message(message, shift):
-    alphabet = ['A','B','C','D','E','F','G','H','I','J','K'
-     ,'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y',
-    'Z']
-    partialstart = ''
-    partialend = ""
-    shiftedAlphabet = ""
-    encodemensage = ''
-   
+    alphabet = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
+    partialStart = ''
+    partialEnd = ''
+    shiftedAlphabet = ''
+    encodedMessage = ''
+
     if shift == 0:
         shiftedAlphabet = alphabet
     elif shift > 0 and shift < 26:
-        partialstart = alphabet [:shift]
-        partialend = alphabet [shift:]
-        shiftedAlphabet = partialend + partialstart
+        partialStart = alphabet[:shift]
+        partialEnd = alphabet[shift:]
+        shiftedAlphabet = partialEnd + partialStart
     elif shift < 0 and shift > -26:
-        shift = shift * -1
-        partialstart = alphabet [:shift]
-        partialend = alphabet [shift:]
-        shiftedAlphabet = partialend + partialstart
+        shift = shift * (-1)
+        partialStart = alphabet[:shift]
+        partialEnd = alphabet[shift:]
+        shiftedAlphabet = partialEnd + partialStart
     else:
         return("shift_error")
+    
     for letter in message:
-        letter_index = alphabet.index(letter.uper)
-        encodemensage = encodemensage + shiftedAlphabet[letter_index]
+        letter_index = alphabet.index(letter.upper())
+        encodedMessage = encodedMessage + shiftedAlphabet[letter_index]
 
-    return (encodemensage)
+    return(encodedMessage)
